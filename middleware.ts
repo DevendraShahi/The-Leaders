937dd0905c95c6da48e75c89a6d76495bff18834
Devendra Shahi Thakuri <devendrashahi304@gmail.com>
2026-02-04 21:49:29 -0500

    Fix Build: Remove empty middleware.ts causing NFT generation error

--- middleware.ts
+++
@@ -1,4 +0,0 @@
-// Middleware temporarily disabled due to Turbopack NFT generation issues
-// TODO: Re-enable middleware after Vercel/Next.js resolves this issue
-
-export const middleware = () => {};
